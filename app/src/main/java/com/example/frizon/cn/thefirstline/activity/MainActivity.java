7a96c24ea4fb36dd86a667579c3c917fd563ee79
EvilBandit <739146920@qq.com>
2016-08-30 17:44:06 +0800

    First Commit

---
+++ app/src/main/java/com/example/frizon/cn/thefirstline/activity/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.frizon.cn.thefirstline.activity;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Message;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.frizon.cn.thefirstline.R;
+
+/**
+ * Created by aili on 2016/8/27.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
